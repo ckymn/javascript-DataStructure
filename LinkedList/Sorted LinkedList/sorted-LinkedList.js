e9d7f90c0dda6a75a848f605a8b2087e06a7c3b1
ckymn <cokyamanmuhammet@gmail.com>
2021-03-12 10:01:32 +0300

    sorted ...

---
+++ LinkedList/Sorted LinkedList/sorted-LinkedList.js
@@ -0,0 +1,52 @@
+const LinkedList = require("../Singly Linked List/1.Singly-LinkedList");
+
+class SortedLinkedList extends LinkedList {
+  constructor(sortingFunction = null) {
+    super();
+    this._sortingFunction = sortingFunction;
+
+    if (!sortingFunction || typeof sortingFunction !== "function") {
+      this._sortingFunction = (a, b) => {
+        if (a === b) return 0;
+
+        return a > b ? 1 : -1;
+      };
+    }
+
+    this.push = undefined;
+  }
+
+  insert(element) {
+    if (this.isEmpty) {
+      return super.insert(element);
+    }
+
+    const index = this._getNextElementIndex(element);
+
+    return super.insert(element, index);
+  }
+
+  _getNextElementIndex(element) {
+    let current = this.head;
+    let i = 0;
+
+    for (; i < this.size; i++) {
+      const res = this._sortingFunction(element, current.element);
+
+      if (res === false || res === -1) return i;
+
+      current = current.next;
+    }
+
+    return i;
+  }
+}
+
+
+const list = new SortedLinkedList();
+
+list.insert(34);
+list.insert(45);
+list.insert(2);
+
+list.print();
