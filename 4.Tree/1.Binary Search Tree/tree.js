727609a1e80c5bf902e184523e3dec13215615c7
ckymn <cokyamanmuhammet@gmail.com>
2021-03-18 10:36:03 +0300

    code fixed

--- 4.Tree/1.Binary Search Tree/tree.js
+++ 4.Tree/1.Binary Search Tree/tree.js
@@ -67,7 +67,7 @@ class BST {
   isPresent(data){
     let current = this.root;
     while(current){
-      if(data === current.value)
+      if(data === current.value) 
         return true;
       if(data < current.data)
         current = current.left;
@@ -167,9 +167,5 @@ let bst = new BST();
 
 bst.insert(4);
 bst.insert(2);
-bst.insert(6);
-bst.insert(1);
-bst.insert(3);
-bst.insert(5);
-bst.insert(7); 
 console.log(bst.postOrder());
+console.log(bst.isPresent(2))
